e2c71483b38528c7647bbe8e06b393f5681c64f7
Kiprov <118040079+Kiprov@users.noreply.github.com>
2025-06-06 18:27:37 +0300

    Update 2.lua

--- Scripts/Laptop Boss/Phases/2.lua
+++ Scripts/Laptop Boss/Phases/2.lua
@@ -1,321 +1,62 @@
 leppie = script.Parent
-	TS = game:GetService("TweenService")
-	keyboard = script.Parent.Keyboard
-	screen = script.Parent.Screen
-	top = script.Parent.Toppart
-	eyes = leppie.Screen.eyes
-	face = leppie.Screen.face
-	mouth = leppie.Screen.mouth
-	db = false
+TS = game:GetService("TweenService")
+keyboard = script.Parent.Keyboard
+screen = script.Parent.Screen
+top = script.Parent.Toppart
+eyes = leppie.Screen.eyes
+face = leppie.Screen.face
+mouth = leppie.Screen.mouth
+db = false
+static = _G.static
+attacks = _G.Attacks
 
-	wait(2)
-	leppie.fite2:Play()
-	wait(2)
-	for kek2 = 1,10 do
-		local etecc2 = Instance.new("Part", leppie)
-		etecc2.BrickColor = BrickColor.Random()
-		etecc2.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc2.Anchored = false
-		etecc2.CanCollide = true
-		etecc2.Shape = "Ball"
-		etecc2.Position = Vector3.new(math.random(-200,200),200, math.random(-200, 200))
-		wait()
-		local etecc3 = Instance.new("Part", leppie)
-		etecc3.BrickColor = BrickColor.Random()
-		etecc3.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc3.Anchored = false
-		etecc3.CanCollide = true
-		etecc3.Shape = "Ball"
-		etecc3.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc4 = Instance.new("Part", leppie)
-		etecc4.BrickColor = BrickColor.Random()
-		etecc4.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc4.Anchored = false
-		etecc4.CanCollide = true
-		etecc4.Shape = "Ball"
-		etecc4.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc5 = Instance.new("Part", leppie)
-		etecc5.BrickColor = BrickColor.Random()
-		etecc5.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc5.Anchored = false
-		etecc5.CanCollide = true
-		etecc5.Shape = "Ball"
-		etecc5.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc6 = Instance.new("Part", leppie)
-		etecc6.BrickColor = BrickColor.Random()
-		etecc6.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc6.Anchored = false
-		etecc6.CanCollide = true
-		etecc6.Shape = "Ball"
-		etecc6.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc7 = Instance.new("Part", leppie)
-		etecc7.BrickColor = BrickColor.Random()
-		etecc7.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc7.Anchored = false
-		etecc7.CanCollide = true
-		etecc7.Shape = "Ball"
-		etecc7.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc8 = Instance.new("Part", leppie)
-		etecc8.BrickColor = BrickColor.Random()
-		etecc8.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc8.Anchored = false
-		etecc8.CanCollide = true
-		etecc8.Shape = "Ball"
-		etecc8.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc9 = Instance.new("Part", leppie)
-		etecc9.BrickColor = BrickColor.Random()
-		etecc9.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc9.Anchored = false
-		etecc9.CanCollide = true
-		etecc9.Shape = "Ball"
-		etecc9.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc10 = Instance.new("Part", leppie)
-		etecc10.BrickColor = BrickColor.Random()
-		etecc10.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc10.Anchored = false
-		etecc10.CanCollide = true
-		etecc10.Shape = "Ball"
-		etecc10.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc11 = Instance.new("Part", leppie)
-		etecc11.BrickColor = BrickColor.Random()
-		etecc11.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc11.Anchored = false
-		etecc11.CanCollide = true
-		etecc11.Shape = "Ball"
-		etecc11.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc12 = Instance.new("Part", leppie)
-		etecc12.BrickColor = BrickColor.Random()
-		etecc12.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc12.Anchored = false
-		etecc12.CanCollide = true
-		etecc12.Shape = "Ball"
-		etecc12.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc13 = Instance.new("Part", leppie)
-		etecc13.BrickColor = BrickColor.Random()
-		etecc13.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc13.Anchored = false
-		etecc13.CanCollide = true
-		etecc13.Shape = "Ball"
-		etecc13.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc14 = Instance.new("Part", leppie)
-		etecc14.BrickColor = BrickColor.Random()
-		etecc14.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc14.Anchored = false
-		etecc14.CanCollide = true
-		etecc14.Shape = "Ball"
-		etecc14.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc15 = Instance.new("Part", leppie)
-		etecc15.BrickColor = BrickColor.Random()
-		etecc15.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc15.Anchored = false
-		etecc15.CanCollide = true
-		etecc15.Shape = "Ball"
-		etecc15.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc16 = Instance.new("Part", leppie)
-		etecc16.BrickColor = BrickColor.Random()
-		etecc16.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc16.Anchored = false
-		etecc16.CanCollide = true
-		etecc16.Shape = "Ball"
-		etecc16.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc17 = Instance.new("Part", leppie)
-		etecc17.BrickColor = BrickColor.Random()
-		etecc17.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc17.Anchored = false
-		etecc17.CanCollide = true
-		etecc17.Shape = "Ball"
-		etecc17.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc18 = Instance.new("Part", leppie)
-		etecc18.BrickColor = BrickColor.Random()
-		etecc18.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc18.Anchored = false
-		etecc18.CanCollide = true
-		etecc18.Shape = "Ball"
-		etecc18.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc19 = Instance.new("Part", leppie)
-		etecc19.BrickColor = BrickColor.Random()
-		etecc19.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc19.Anchored = false
-		etecc19.CanCollide = true
-		etecc19.Shape = "Ball"
-		etecc19.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		local etecc20 = Instance.new("Part", leppie)
-		etecc20.BrickColor = BrickColor.Random()
-		etecc20.Size = Vector3.new(5.59, 4.4, 4.71)
-		etecc20.Anchored = false
-		etecc20.CanCollide = true
-		etecc20.Shape = "Ball"
-		etecc20.Position = Vector3.new(math.random(-300,300),300, math.random(-300, 300))
-		wait()
-		wait(3)
-		local boom = Instance.new("Explosion", etecc2)
-		boom.Position = etecc2.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc3)
-		boom.Position = etecc3.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc4)
-		boom.Position = etecc4.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc5)
-		boom.Position = etecc5.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc6)
-		boom.Position = etecc6.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc7)
-		boom.Position = etecc7.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc8)
-		boom.Position = etecc8.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc9)
-		boom.Position = etecc9.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc10)
-		boom.Position = etecc10.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc11)
-		boom.Position = etecc11.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc12)
-		boom.Position = etecc12.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc13)
-		boom.Position = etecc13.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc14)
-		boom.Position = etecc14.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc15)
-		boom.Position = etecc15.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc16)
-		boom.Position = etecc16.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc17)
-		boom.Position = etecc17.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc18)
-		boom.Position = etecc18.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc19)
-		boom.Position = etecc19.Position
-		boom.BlastRadius = 30
-		wait()
-		local boom = Instance.new("Explosion", etecc20)
-		boom.Position = etecc20.Position
-		boom.BlastRadius = 30
-		wait()
-		etecc2:Destroy()
-		etecc3:Destroy()
-		etecc4:Destroy()
-		etecc5:Destroy()
-		etecc6:Destroy()
-		etecc7:Destroy()
-		etecc8:Destroy()
-		etecc9:Destroy()
-		etecc10:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc11:Destroy()
-		etecc12:Destroy()
-		etecc13:Destroy()
-		etecc14:Destroy()
-		etecc15:Destroy()
-		etecc16:Destroy()
-		etecc17:Destroy()
-		etecc18:Destroy()
-		etecc19:Destroy()
-		etecc20:Destroy()
-		wait(3)
-	end
-	local weakspot = Instance.new("Part", keyboard)
-	weakspot.Shape = "Cylinder"
-	weakspot.BrickColor = BrickColor.new("Crimson")
-	weakspot.Position = keyboard.Position + Vector3.new(0, 1, 0)
-	weakspot.Size = Vector3.new(0.85, 8.96, 3.03)
-	weakspot.Anchored = true
-	weakspot.CanCollide = true
-	weakspot.Orientation = Vector3.new(0,0,90)
-	weakspot.Transparency = 1
-	wait(1)
-	game.TweenService:Create(weakspot, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out),{Transparency = 0}):Play()
-
-	function onTouch2()
-		if db then return end
-		db = true
-		print("weakspot has been attacked!")
-		task.spawn(function()
-			local waitTween = game.TweenService:Create(weakspot, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out),{Transparency = 1})
-			waitTween:Play()
-			waitTween.Completed:Wait()
-			weakspot:Destroy()
+wait(2)
+leppie.fite2:Play()
+wait(2)
+for kek2 = 1,10 do
+	for i = 1,20 do
+		spawn(function()
+			attacks.Bomb()
 		end)
-		leppie.fite2:Stop()
-		leppie.pain.Pitch = 0.5
-		leppie.pain:Play()
-		leppie.crash:Play()
-		screen.breek2.Transparency = 0
-		screen.face.Texture = "http://www.roblox.com/asset/?id=132894111"
-		wait(7)
-		leppie.static:Play()
-		face.Texture = "http://www.roblox.com/asset/?id=1437986267"
-		wait(0.1)
-		face.Texture = "http://www.roblox.com/asset/?id=161123846"
-		wait(0.1)
-		face.Texture = "http://www.roblox.com/asset/?id=1437986267"
-		wait(0.1)
-		face.Texture = "http://www.roblox.com/asset/?id=161123846"
-		wait(0.1)
-		leppie.static:Stop()
-		face.Texture = "http://www.roblox.com/asset/?id=301890436"
-		wait(2)
-		leppie.Phase2.Disabled = true
-		db = false
-		local phase3 = NS(_G.Phases[3],leppie)
-		phase3.Name = "Phase3"
 	end
+	wait(6)
+end
+local weakspot = Instance.new("Part", keyboard)
+weakspot.Shape = "Cylinder"
+weakspot.BrickColor = BrickColor.new("Crimson")
+weakspot.Position = keyboard.Position + Vector3.new(0, 1, 0)
+weakspot.Size = Vector3.new(0.85, 8.96, 3.03)
+weakspot.Anchored = true
+weakspot.CanCollide = true
+weakspot.Orientation = Vector3.new(0,0,90)
+weakspot.Transparency = 1
+wait(1)
+game.TweenService:Create(weakspot, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out),{Transparency = 0}):Play()
 
-	weakspot.Touched:connect(onTouch2)
+function onTouch2()
+	if db then return end
+	db = true
+	print("weakspot has been attacked!")
+	task.spawn(function()
+		local waitTween = game.TweenService:Create(weakspot, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out),{Transparency = 1})
+		waitTween:Play()
+		waitTween.Completed:Wait()
+		weakspot:Destroy()
+	end)
+	leppie.fite2:Stop()
+	leppie.pain.Pitch = 0.5
+	leppie.pain:Play()
+	leppie.crash:Play()
+	screen.breek2.Transparency = 0
+	screen.face.Texture = "http://www.roblox.com/asset/?id=132894111"
+	wait(7)
+	static(2)
+	face.Texture = "http://www.roblox.com/asset/?id=301890436"
+	wait(2)
+	leppie.Phase2.Disabled = true
+	local phase3 = NS(_G.Phases[3],leppie)
+	phase3.Name = "Phase3"
+	db = false
+end
+
+weakspot.Touched:connect(onTouch2)
